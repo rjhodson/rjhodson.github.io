a20060ab7d9131677026fbb43a7844fc15869f4f
Richard Hodson <rjhodson@gmail.com>
2020-02-04 22:27:18 -0500

    saving theme state

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,40 @@
+/**
+ * icons
+ */
 feather.replace();
 
+/**
+ * establish theme toggle
+ */
 const toggleSwitch = document.querySelector('#theme-toggle');
-const toggleButtons = toggleSwitch.querySelectorAll('svg');
 
 const switchTheme = (e) => {
-    e.preventDefault();
+    if(e) e.preventDefault();
 
     document.body.classList.toggle('theme-light');
     document.body.classList.toggle('theme-dark');
 
-    toggleButtons.forEach((button) => {
-        button.classList.toggle('block');
-        button.classList.toggle('hidden');
-    });
+    if(document.body.classList.contains('theme-light')) {
+        window.localStorage.setItem("theme", 'light');
+    } else {
+        window.localStorage.setItem("theme", 'dark');
+    }
 
     return false;
 }
 
 toggleSwitch.addEventListener('click', switchTheme, false);
+
+/**
+ * set theme to dark on page load if saved dark
+ */
+if (window.localStorage.getItem("theme") == 'dark') {
+    document.body.classList.remove('content-wrapper');
+    switchTheme();
+    document.body.classList.add('content-wrapper');
+}
+
+/**
+ * unhide page
+ */
+document.body.classList.remove('hidden');
